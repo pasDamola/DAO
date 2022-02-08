dedbf1e605c1743985ec8b0c2dff64a5cd13d909
pasDamola <oyindamola.oyetunmibi@otusourceglobal.com>
2022-02-08 08:19:34 +0100

    updates

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xeF851a75D8CE44eBd27031B242f70dbcda9732D6";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
